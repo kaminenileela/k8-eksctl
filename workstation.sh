cca0e09ea2e9e5523ed5f5b8ea4e4fc3aef3a1cf
kaminenileela <kaminenileela1992@email.com>
2024-12-12 12:17:56 -0500

    expense

--- workstation.sh
+++ workstation.sh
@@ -40,7 +40,7 @@ VALIDATE $? "Docker installation"
 # eksctl
 curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
 mv /tmp/eksctl /usr/local/bin
-eksctl version
+/usr/local/bin/eksctl version
 VALIDATE $? "eksctl installation"
 
 
